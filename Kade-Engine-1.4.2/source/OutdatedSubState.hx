f69b9301783d31db17ea019a2dd1690f21cebbe5
PlasmaOJ15 <orlandorodriquezjr@gmail.com>
2021-07-16 21:36:19 -0700

    files here

---
+++ Kade-Engine-1.4.2/source/OutdatedSubState.hx
@@ -0,0 +1,44 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class OutdatedSubState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	public static var needVer:String = "IDFK LOL";
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"This mod is based off of the game Like A Dino\n"
+			+ "Press enter to be taken to the game's store page, or press escape to continue! "
+			+ "\n I hope you enjoy my mod! - PlasmaOJ15",
+			32);
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		txt.screenCenter();
+		add(txt);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			FlxG.openURL("https://play.google.com/store/apps/details?id=com.super_toki.giraffe&hl=en_US&gl=US");
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}
